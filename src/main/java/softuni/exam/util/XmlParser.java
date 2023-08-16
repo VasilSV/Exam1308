a82addfa3c59400e6789f90d9d32538392e69043
VasilSV <114056331+VasilSV@users.noreply.github.com>
2023-08-16 20:42:00 +0300

    Revert "Initial commit"
    This reverts commit d507125cf01dc5f075e167d449b9ede73d8e3df2.

--- src/main/java/softuni/exam/util/XmlParser.java
+++
@@ -1,8 +0,0 @@
-package softuni.exam.util;
-
-import javax.xml.bind.JAXBException;
-import java.io.FileNotFoundException;
-
-public interface XmlParser {
-    <T> T fromFile(String path, Class<T> tClass) throws JAXBException, FileNotFoundException;
-}
